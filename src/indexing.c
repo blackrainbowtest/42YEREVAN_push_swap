953ebc815031b00f9f875feaed09899c4cf4b861
blackrainbowtest <araqelyan.aram.96@gmail.com>
2025-05-10 22:22:24 +0400

    ADD

---
+++ src/indexing.c
@@ -0,0 +1,30 @@
+#include <pushswap.h>
+
+void	bubble_sort(int *arr, int len)
+{
+	int	i;
+	int	j;
+	int	tmp;
+	int	is_sorted;
+
+	i = 0;
+	while (i < len - 1)
+	{
+		j = 0;
+		is_sorted = 0;
+		while (j < len - i - 1)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				tmp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = tmp;
+				is_sorted = 1;
+			}
+			j++;
+		}
+		if (!is_sorted)
+			break;
+		i++;
+	}
+}
