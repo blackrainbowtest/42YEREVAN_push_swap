f0fe8ffc94fb56349a38f84042d63ec6c3374f68
blackrainbowtest <araqelyan.aram.96@gmail.com>
2025-05-15 21:37:42 +0400

    add

--- src/op_swap.c
+++ src/op_swap.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   op_swap.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aramarak <aramarak@student.42.fr>          +#+  +:+       +#+        */
+/*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 19:33:45 by aramarak          #+#    #+#             */
-/*   Updated: 2025/05/12 20:35:40 by aramarak         ###   ########.fr       */
+/*   Updated: 2025/05/15 21:37:34 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,45 @@
 
 void	sa(t_data *data)
 {
-	// Swap the first two elements of stack A
+	t_stack	*first;
+	t_stack	*second;
+
+	if (!data->a || !data->a->next)
+		return ;
+	first = data->a;
+	second = data->a->next;
+	first->next = second->next;
+	if (second->next)
+		second->next->prev = first;
+	second->prev = NULL;
+	second->next = first;
+	first->prev = second;
+	data->a = second;
+	write(1, "sa\n", 3);
 }
 
 void	sb(t_data *data)
 {
-	// Swap the first two elements of stack B
+	t_stack	*first;
+	t_stack	*second;
+
+	if (!data->b || !data->b->next)
+		return ;
+	first = data->b;
+	second = data->b->next;
+	first->next = second->next;
+	if (second->next)
+		second->next->prev = first;
+	second->prev = NULL;
+	second->next = first;
+	first->prev = second;
+	data->b = second;
+	write(1, "sb\n", 3);
 }
 
 void	ss(t_data *data)
 {
-	// sa and sb at the same time
+	sa(data);
+	sb(data);
+	write(1, "ss\n", 3);
 }
